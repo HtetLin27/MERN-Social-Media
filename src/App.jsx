3f3300a3e9542133f2d4b8482ff2776af58fb16b
htetlynn <htetlynnko27@gmail.com>
2024-12-22 22:38:25 +0700

    form edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { useState } from "react";
 import Item from "./Item";
 import List from "./List";
+import From from "./From";
 
 export default function App() {
+  const [showForm, setShowForm] = useState(false);
   const [data, setData] = useState([
     { id: 1, content: "Hello, World", name: "Alice" },
     { id: 2, content: "React is fun", name: "Bob" },
@@ -13,9 +15,36 @@ export default function App() {
     console.log(id);
     setData((prev) => prev.filter((item) => item.id !== id));
   };
+
+  const add = (content, name) => {
+    const id = data[data.length - 1].id + 1;
+    setData([...data, { id, content, name }]);
+  };
   return (
     <div style={{ maxWidth: 600, margin: "20px auto" }}>
-      <h1>Yaycha</h1>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        Yaycha
+        <button
+          onClick={() => setShowForm(!showForm)}
+          style={{
+            width: 32,
+            height: 32,
+            borderRadius: 50,
+            border: "0 none",
+            background: showForm ? "#dc3545" : "#0d6efd",
+            color: "white",
+          }}
+        >
+          {showForm ? "x" : "+"}
+        </button>
+      </h1>
+      {showForm && <From add={add} />}
       <List>
         {data.map((item) => (
           <Item key={item.id} item={item} remove={remove} />
---
+++ src/From.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useRef } from "react";
+
+function From({ add }) {
+  const contentRef = useRef();
+  const nameRef = useRef();
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        const content = contentRef.current.value;
+        const name = nameRef.current.value;
+        add(content, name);
+        e.currentTarget.reset();
+      }}
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: 3,
+        padding: 10,
+        borderRadius: 8,
+        marginBottom: 20,
+        background: "#def",
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Content"
+        ref={contentRef}
+        style={{ padding: 5 }}
+      />
+      <input
+        type="text"
+        placeholder="Name"
+        ref={nameRef}
+        style={{ padding: 5 }}
+      />
+      <button
+        type="submit"
+        style={{
+          padding: 8,
+          background: "#0d6efd",
+          color: "white",
+          border: "0 none",
+        }}
+      >
+        Post
+      </button>
+    </form>
+  );
+}
+
+export default From;
